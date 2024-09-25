7855f8a6c449ce9a909d9bbf5426a83e556b978b
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-09-25 21:26:38 +0300

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'webserver',
+    'python-project-52-u7be.onrender.com'
+]
 
 
 # Application definition
