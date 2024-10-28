1777fb78c220bfe2e0ed009d0735ff1b4daf94ed
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-28 17:07:12 +0300

    Create statuses/forms.py

---
+++ task_manager/statuses/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from task_manager.statuses.models import Status
+
+
+class StatusCreationForm(ModelForm):
+    class Meta:
+        model = Status
+        fields = ['name']
+
+
+class StatusChangeForm:
+    pass
