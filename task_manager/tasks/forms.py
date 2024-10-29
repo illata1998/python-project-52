7fec4b3c51eb54d56e57b04b3b68dc55efdcc3b2
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-29 20:31:25 +0300

    Update forms

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+
 from task_manager.tasks.models import Task
 
 
--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, BaseUserCreationForm
+
 from task_manager.users.models import User
 
 
