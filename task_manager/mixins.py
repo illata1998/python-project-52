10bb8ed837cb13a7ea05d8ddf43ce5b6756962f2
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-29 20:31:32 +0300

    Update mixins.py

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,16 +1,19 @@
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.urls import reverse_lazy
-from django.shortcuts import redirect
-from django.utils.translation import gettext_lazy as _
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomLoginRequiredMixin(LoginRequiredMixin):
     login_url = reverse_lazy('login')
     redirect_field_name = None
+    access_denied_message = _('You are not authorized! Please, log in.')
+
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            messages.add_message(request, messages.ERROR, _('You are not authorized! Please, log in.'))
+            messages.add_message(request, messages.ERROR,
+                                 self.access_denied_message)
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
 
@@ -19,13 +22,15 @@ class UserPermissionMixin(UserPassesTestMixin):
     permission_denied_url = None
     redirect_field_name = None
     permission_denied_message = ''
+
     def test_func(self):
         return self.get_object() == self.request.user
 
     def dispatch(self, request, *args, **kwargs):
         user_test_result = self.get_test_func()()
         if not user_test_result:
-            messages.add_message(request, messages.ERROR, self.permission_denied_message)
+            messages.add_message(request, messages.ERROR,
+                                 self.permission_denied_message)
             return redirect(self.permission_denied_url)
         return super().dispatch(request, *args, **kwargs)
 
@@ -34,12 +39,14 @@ class AuthorPermissionMixin(UserPassesTestMixin):
     permission_denied_url = None
     redirect_field_name = None
     permission_denied_message = ''
+
     def test_func(self):
         return self.get_object().author == self.request.user
 
     def dispatch(self, request, *args, **kwargs):
         user_test_result = self.get_test_func()()
         if not user_test_result:
-            messages.add_message(request, messages.ERROR, self.permission_denied_message)
+            messages.add_message(request, messages.ERROR,
+                                 self.permission_denied_message)
             return redirect(self.permission_denied_url)
         return super().dispatch(request, *args, **kwargs)
