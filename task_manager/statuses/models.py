ab90a2912f0a5a43116181ab633386192d824bb3
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-29 20:31:39 +0300

    Update models.py

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+
 class Status(models.Model):
     name = models.CharField(verbose_name=_('Name'), unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
 from task_manager.statuses.models import Status
 from task_manager.users.models import User
+
 
 class Task(models.Model):
     name = models.CharField(blank=False, unique=True, verbose_name=_('Name'))
     description = models.TextField(blank=False, verbose_name=_('Description'))
-    author = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name=_('Author'), related_name='Author')
-    status = models.ForeignKey(Status, on_delete=models.PROTECT, verbose_name=_('Status'))
-    executor = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name=_('Executor'), related_name='Executor')
+    author = models.ForeignKey(User, on_delete=models.PROTECT,
+                               verbose_name=_('Author'), related_name='Author')
+    status = models.ForeignKey(Status, on_delete=models.PROTECT,
+                               verbose_name=_('Status'))
+    executor = models.ForeignKey(User, on_delete=models.PROTECT,
+                                 verbose_name=_('Executor'),
+                                 related_name='Executor')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
