1d23cce9148f070eec3e80f0a729c14015da45e4
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-29 09:27:18 +0300

    Start the tasks app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -51,7 +51,8 @@ INSTALLED_APPS = [
     'django_bootstrap5',
     'task_manager',
     'task_manager.users',
-    'task_manager.statuses'
+    'task_manager.statuses',
+    'task_manager.tasks'
 ]
 
 MIDDLEWARE = [
