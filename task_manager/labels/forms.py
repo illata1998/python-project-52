9b989498abae57503c824946d9548f6cf1eefb57
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-30 10:34:10 +0300

    Update label creation form

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from task_manager.labels.models import Label
 
 
-class StatusCreationForm(ModelForm):
+class LabelCreationForm(ModelForm):
     class Meta:
         model = Label
         fields = ['name']
