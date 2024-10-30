c88c0951d6de48e85475d4c40e5ef06cf49b931a
Ulyana Krasilnikova <b-lack-u@yandex.ru>
2024-10-30 10:30:15 +0300

    Create label model

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Label(models.Model):
+    name = models.CharField(verbose_name=_('Name'), unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
